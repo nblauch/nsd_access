defa6dc55ef2b3977d07a701b25f94d6d709e4f9
nblauch <blauch@cmu.edu>
2021-09-28 08:28:49 -0400

    autodetect when to use agg backend

--- nsd_access/__init__.py
+++ nsd_access/__init__.py
@@ -1,1 +1,24 @@
 from .nsda import *
+
+def X_is_running():
+    from subprocess import Popen, PIPE
+    p = Popen(["xset", "-q"], stdout=PIPE, stderr=PIPE)
+    p.communicate()
+    return p.returncode == 0
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+if not X_is_running() and not isnotebook():
+    print('no X server detected. changing default matplotlib backend to Agg for compatibility.')
+    import matplotlib
+    matplotlib.use('Agg')
